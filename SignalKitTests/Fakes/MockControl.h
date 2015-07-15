986db97cafd83b8a25c4120eb75d5d4092062458
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-07-15 16:21:41 +0300

    Added ControlObserver

---
+++ SignalKitTests/Fakes/MockControl.h
@@ -0,0 +1,17 @@
+//
+//  MockControl.h
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 7/15/15.
+//  Copyright (c) 2015 Yanko Dimitrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#ifndef Observable_MockControl_h
+#define Observable_MockControl_h
+
+@interface MockControl : UIControl
+@end
+
+#endif
---
+++ SignalKitTests/Fakes/MockControl.m
@@ -0,0 +1,29 @@
+//
+//  MockControl.m
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 7/15/15.
+//  Copyright (c) 2015 Yanko Dimitrov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "MockControl.h"
+
+@implementation MockControl
+
+-(void)sendAction:(SEL)action to:(id)target forEvent:(UIEvent *)event {
+    
+    //
+    // Thanks to ReactiveCocoa!
+    // https://github.com/ReactiveCocoa/ReactiveCocoa
+    //
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    
+    [target performSelector:action withObject:nil];
+    
+    #pragma clang diagnostic pop
+}
+
+@end
