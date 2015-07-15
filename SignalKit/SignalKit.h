32b490bd7c80ae8dbd9946adfbf363529c26c7a6
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-07-15 14:41:59 +0300

    Initial Commit

---
+++ SignalKit/SignalKit.h
@@ -0,0 +1,19 @@
+//
+//  SignalKit.h
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 7/15/15.
+//  Copyright (c) 2015 Yanko Dimitrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SignalKit.
+FOUNDATION_EXPORT double SignalKitVersionNumber;
+
+//! Project version string for SignalKit.
+FOUNDATION_EXPORT const unsigned char SignalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalKit/PublicHeader.h>
+
+
