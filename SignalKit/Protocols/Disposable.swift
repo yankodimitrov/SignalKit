3ea5353d7fee6d6578ad603c421a2f29e37b4caa
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-07-15 14:57:55 +0300

    Added Disposable

---
+++ SignalKit/Protocols/Disposable.swift
@@ -0,0 +1,14 @@
+//
+//  Disposable.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 7/15/15.
+//  Copyright (c) 2015 Yanko Dimitrov. All rights reserved.
+//
+
+import Foundation
+
+public protocol Disposable {
+    
+    func dispose()
+}
