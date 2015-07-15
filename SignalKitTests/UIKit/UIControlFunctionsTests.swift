3f2406c69d5ca9a51fd44d0a1a6389e825f7e7b4
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-07-15 17:03:31 +0300

    Added UIControlFunctions

---
+++ SignalKitTests/UIKit/UIControlFunctionsTests.swift
@@ -0,0 +1,67 @@
+//
+//  UIControlFunctionsTests.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 7/15/15.
+//  Copyright (c) 2015 Yanko Dimitrov. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class UIControlFunctionsTests: XCTestCase {
+
+    var control: MockControl!
+    var signalContainer: SignalContainer!
+    
+    override func setUp() {
+        super.setUp()
+        
+        control = MockControl()
+        signalContainer = SignalContainer()
+    }
+    
+    /// MARK: Observe UIControl
+    
+    func testObserveControlForControlEvents() {
+        
+        var called = false
+        
+        observe(control, forEvents: .ValueChanged)
+            .next { _ in called = true }
+            .addTo(signalContainer)
+        
+        control.sendActionsForControlEvents(.ValueChanged)
+        
+        XCTAssertEqual(called, true, "Should observe the UIControl for UIControlEvents")
+    }
+    
+    func testObserveControlForControlEventsCallback() {
+        
+        var called = false
+        
+        let signal = observe(control, forEvents: .ValueChanged) {
+            _ in
+            
+            called = true
+        }
+        
+        control.sendActionsForControlEvents(.ValueChanged)
+        
+        XCTAssertEqual(called, true, "Should call the observation callback")
+    }
+    
+    func testBindToIsEnabled() {
+        
+        let state = ObservableOf<Bool>()
+        control.enabled = false
+        
+        observe(state)
+            .bindTo(isEnabled(control))
+            .addTo(signalContainer)
+        
+        state.dispatch(true)
+        
+        XCTAssertEqual(control.enabled, true, "Should bind a boolean value to the enabled state of UIControl")
+    }
+}
