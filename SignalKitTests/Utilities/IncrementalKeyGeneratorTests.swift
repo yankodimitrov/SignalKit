53a41a4679df6a32eac073b4e04b5a6f7d0134f6
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-08-12 09:10:34 +0300

    Added IncrementalKeyGenerator

---
+++ SignalKitTests/Utilities/IncrementalKeyGeneratorTests.swift
@@ -0,0 +1,33 @@
+//
+//  IncrementalKeyGeneratorTests.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 8/12/15.
+//  Copyright © 2015 Yanko Dimitrov. All rights reserved.
+//
+
+import XCTest
+
+class IncrementalKeyGeneratorTests: XCTestCase {
+    
+    func testGenerateToken() {
+        
+        var generator = IncrementalKeyGenerator()
+        
+        let token = generator.nextToken()
+        
+        XCTAssertEqual(token, "1", "First generated token should be equal to 1")
+    }
+    
+    func testGenerateIncrementalTokens() {
+        
+        var generator = IncrementalKeyGenerator(stepLimit: 2)
+        
+        generator.nextToken() // "1"
+        generator.nextToken() // "2"
+        
+        let token = generator.nextToken() // step limit is 2 -> "21"
+        
+        XCTAssertEqual(token, "21", "Should generate incremental sequence of tokens")
+    }
+}
