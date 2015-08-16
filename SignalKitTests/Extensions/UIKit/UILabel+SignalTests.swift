6cf38eed57b8e4daa185a5325fa982e67bb6ed63
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-08-16 13:24:06 +0300

    Added UILabel extensions

---
+++ SignalKitTests/Extensions/UIKit/UILabel+SignalTests.swift
@@ -0,0 +1,45 @@
+//
+//  UILabel+SignalTests.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 8/16/15.
+//  Copyright © 2015 Yanko Dimitrov. All rights reserved.
+//
+
+import XCTest
+
+class UILabel_SignalTests: XCTestCase {
+    
+    var label: UILabel!
+    var signalsBag: SignalBag!
+    
+    override func setUp() {
+        super.setUp()
+        
+        label = UILabel()
+        signalsBag = SignalBag()
+    }
+    
+    func testBindToText() {
+        
+        let signal = MockSignal<String>()
+        
+        signal.dispatch("Jack")
+        
+        signal.bindTo(textIn: label).addTo(signalsBag)
+        
+        XCTAssertEqual(label.text, "Jack", "Should bind a String value to the text property of UILabel")
+    }
+    
+    func testBindToAttributedText() {
+        
+        let signal = MockSignal<NSAttributedString>()
+        let attributedString = NSAttributedString(string: "Jack")
+        
+        signal.dispatch(attributedString)
+        
+        signal.bindTo(attributedTextIn: label).addTo(signalsBag)
+        
+        XCTAssertEqual(label.attributedText!, attributedString, "Should bind a NSAttributedString to the attributedText property of UILabel")
+    }
+}
