6f7a2d423d20781b9de4a65158f81f8ffe32118a
Yanko Dimitrov <yanko@yankodimitrov.com>
2016-01-20 14:38:03 +0200

    Added CollectionChangeset

---
+++ SignalKitTests/Events/CollectionChangesetTests.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionChangesetTests.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 1/20/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import XCTest
+
+class CollectionChangesetTests: XCTestCase {
+
+}
