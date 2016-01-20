b4af59f36dba460d2e4e81c0fdd8a0c399abf8f6
Yanko Dimitrov <yanko@yankodimitrov.com>
2016-01-20 18:50:38 +0200

    Added ObservableCollectionType protocol

---
+++ SignalKit/Protocols/ObservableCollectionType.swift
@@ -0,0 +1,22 @@
+//
+//  ObservableCollectionType.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 1/20/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import Foundation
+
+public protocol ObservableCollectionType {
+    
+    var changeSetSignal: Signal<CollectionChangeSet> { get }
+}
+
+extension ObservableCollectionType {
+    
+    public func observe() -> SignalEvent<ObservableCollectionType> {
+        
+        return SignalEvent(sender: self)
+    }
+}
