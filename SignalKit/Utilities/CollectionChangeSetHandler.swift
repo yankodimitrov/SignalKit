7567c3cea0692d4ef751f7f4c7059c3bc8c22825
Yanko Dimitrov <yanko@yankodimitrov.com>
2016-01-21 13:00:09 +0200

    Added handleChangeSet(...)

--- SignalKit/Utilities/CollectionChangeSetHandler.swift
+++ SignalKit/Utilities/CollectionChangeSetHandler.swift
@@ -21,3 +21,62 @@ internal final class CollectionChangeSetHandler {
     
     internal init() {}
 }
+
+extension CollectionChangeSetHandler {
+    
+    internal func handleChangeSet(var changeSet: CollectionChangeSet) {
+        
+        guard !changeSet.collectionOperations.contains(.Reset) else {
+            
+            shouldResetCollection = true
+            return
+        }
+        
+        handleSectionsChanges(changeSet)
+        handleItemsChanges(changeSet)
+        
+        shouldResetCollection = false
+    }
+    
+    private func handleSectionsChanges(var changeSet: CollectionChangeSet) {
+        
+        for operation in changeSet.collectionOperations {
+            
+            switch operation {
+            
+            case let .Insert(index): insertedSections.insert(index)
+            case let .Update(index): updatedSections.insert(index)
+            case let .Remove(index): removedSections.insert(index)
+            default: break
+            }
+        }
+    }
+    
+    private func handleItemsChanges(var changeSet: CollectionChangeSet) {
+        
+        for (section, operations) in changeSet.sectionsOperations {
+            
+            handleListOperations(operations, inSection: section)
+        }
+    }
+    
+    private func handleListOperations(operations: Set<ListOperation>, inSection: Int) {
+        
+        for operation in operations {
+            
+            switch operation {
+            
+            case let .Insert(index):
+                insertedIndexPaths.append(NSIndexPath(forItem: index, inSection: inSection))
+            
+            case let .Update(index):
+                updatedIndexPaths.append(NSIndexPath(forItem: index, inSection: inSection))
+            
+            case let .Remove(index):
+                removedIndexPaths.append(NSIndexPath(forItem: index, inSection: inSection))
+            
+            default: break
+            }
+        }
+    }
+}
--- SignalKitTests/Utilities/CollectionChangeSetHandlerTests.swift
+++ SignalKitTests/Utilities/CollectionChangeSetHandlerTests.swift
@@ -11,4 +11,54 @@ import XCTest
 
 class CollectionChangeSetHandlerTests: XCTestCase {
 
+    func testHandleChangeSetWithSectionChanges() {
+        
+        let handler = CollectionChangeSetHandler()
+        var changeSet = CollectionChangeSet()
+        
+        changeSet.insertSectionAtIndex(0)
+        changeSet.updateSectionAtIndex(1)
+        changeSet.removeSectionAtIndex(2)
+        
+        handler.handleChangeSet(changeSet)
+        
+        XCTAssertEqual(handler.insertedSections, Set([0]), "Should contain the inserted")
+        XCTAssertEqual(handler.updatedSections, Set([1]), "Should contain the updated")
+        XCTAssertEqual(handler.removedSections, Set([2]), "Should contain the remvoed")
+        XCTAssertEqual(handler.shouldResetCollection, false, "Should be equal to false")
+    }
+    
+    func testHandleChangeSetCollectionReset() {
+        
+        let handler = CollectionChangeSetHandler()
+        var changeSet = CollectionChangeSet()
+        
+        changeSet.insertSectionAtIndex(0)
+        changeSet.replaceAllSections()
+        
+        handler.handleChangeSet(changeSet)
+        
+        XCTAssertEqual(handler.shouldResetCollection, true, "Should reset the collection")
+    }
+    
+    func testHandlerChangeSetWithItemsChanges() {
+        
+        let handler = CollectionChangeSetHandler()
+        var changeSet = CollectionChangeSet()
+        
+        let expectedInsertedItems = [NSIndexPath(forItem: 0, inSection: 0)]
+        let expectedUpdatedItems = [NSIndexPath(forItem: 1, inSection: 0)]
+        let expectedRemovedItems = [NSIndexPath(forItem: 0, inSection: 2)]
+        
+        changeSet.insertItemAtIndex(0, inSection: 0)
+        changeSet.updateItemAtIndex(1, inSection: 0)
+        changeSet.removeItemAtIndex(0, inSection: 2)
+        
+        handler.handleChangeSet(changeSet)
+        
+        XCTAssertEqual(handler.insertedIndexPaths, expectedInsertedItems, "Should contain the inserted items")
+        XCTAssertEqual(handler.updatedIndexPaths, expectedUpdatedItems, "Should contain the updated items")
+        XCTAssertEqual(handler.removedIndexPaths, expectedRemovedItems, "Should contain the removed itesm")
+        XCTAssertEqual(handler.shouldResetCollection, false, "Should be equal to false")
+    }
 }
