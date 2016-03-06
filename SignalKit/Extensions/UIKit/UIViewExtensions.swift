bada76051a4f27d14ce7dfbb26b6c0b1011f9627
yankodimitrov <qnkod3@gmail.com>
2016-03-06 17:57:45 +0200

    Add UIViewExtensions

---
+++ SignalKit/Extensions/UIKit/UIViewExtensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewExtensions.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 3/6/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+public extension SignalType where ObservationValue == CGFloat {
+    
+    /// Bind a CGFloat value to the alpha property of UIView
+    
+    public func bindTo(alphaIn view: UIView) -> Self {
+        
+        addObserver { [weak view] in
+            
+            view?.alpha = $0
+        }
+        
+        return self
+    }
+}
+
+public extension SignalType where ObservationValue == Bool {
+    
+    /// Bind a boolean value to the hidden property of UIView
+    
+    public func bindTo(hiddenStateIn view: UIView) -> Self {
+        
+        addObserver { [weak view] in
+            
+            view?.hidden = $0
+        }
+        
+        return self
+    }
+}
