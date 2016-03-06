6057277be8bd2bc237e593ff770b02435b767c3b
yankodimitrov <qnkod3@gmail.com>
2016-03-06 17:46:32 +0200

    Add UITextViewExtensions

---
+++ SignalKit/Extensions/UIKit/UITextViewExtensions.swift
@@ -0,0 +1,30 @@
+//
+//  UITextViewExtensions.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 3/6/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+public extension SignalEventType where Sender: UITextView {
+    
+    /// Observe text changes in UITextView
+    
+    var text: Signal<String> {
+        
+        let signal = Signal<String>()
+        let center = NSNotificationCenter.defaultCenter()
+        let observer = NotificationObserver(center: center, name: UITextViewTextDidChangeNotification, object: sender)
+        
+        observer.notificationCallback = { [weak sender, weak signal] _ in
+        
+            signal?.sendNext(sender?.text ?? "")
+        }
+        
+        signal.disposableSource = observer
+        
+        return signal
+    }
+}
