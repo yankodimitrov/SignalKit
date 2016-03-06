2db71c20cf5d18fdda2d6a8fac3512fb68194963
yankodimitrov <qnkod3@gmail.com>
2016-03-06 12:15:53 +0200

    Add NSNotificationCenterExtensions

---
+++ SignalKit/Extensions/NSNotificationCenterExtensions.swift
@@ -0,0 +1,27 @@
+//
+//  NSNotificationCenterExtensions.swift
+//  SignalKit
+//
+//  Created by Yanko Dimitrov on 3/6/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import Foundation
+
+public extension SignalEventType where Sender: NSNotificationCenter {
+    
+    public func notification(name: String, fromObject: AnyObject? = nil) -> Signal<NSNotification> {
+        
+        let signal = Signal<NSNotification>()
+        let observer = NotificationObserver(center: sender, name: name, object: fromObject)
+        
+        observer.notificationCallback = { [weak signal] in
+            
+            signal?.sendNext($0)
+        }
+        
+        signal.disposableSource = observer
+        
+        return signal
+    }
+}
